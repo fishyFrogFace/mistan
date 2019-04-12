0ab9398190f9d9f1d5c2eed1b4e3a6d5f59e8735
Camilla Rygh <camillahallo@mail.com>
2019-04-12 02:36:03 +0200

    NTNU campus entries

---
+++ content.sql
@@ -0,0 +1,13 @@
+INSERT INTO campus VALUES
+  (DEFAULT, 'Gløshaugen'),
+  (DEFAULT, 'Dragvoll'),
+  (DEFAULT, 'Kalvskinnet'),
+  (DEFAULT, 'Øya'),
+  (DEFAULT, 'Tyholt'),
+  (DEFAULT, 'Moholt'),
+  (DEFAULT, 'Ålesund'),
+  (DEFAULT, 'Gjøvik'),
+  (DEFAULT, 'Tunga'),
+  (DEFAULT, 'Valgrinda'),
+  (DEFAULT, 'Olavskvartalet'),
+  (DEFAULT, 'Kunstakademiet');
