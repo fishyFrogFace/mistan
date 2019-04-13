4f3709fdfe0b96e6bb7af4763a7d8c549250d41c
Camilla Rygh <camillahallo@mail.com>
2019-04-13 16:53:49 +0200

    NTNU as company

--- content.sql
+++ content.sql
@@ -1,13 +1,16 @@
+INSERT INTO company VALUES
+  (DEFAULT, 'NTNU');
+
 INSERT INTO campus VALUES
-  (DEFAULT, 'Gløshaugen'),
-  (DEFAULT, 'Dragvoll'),
-  (DEFAULT, 'Kalvskinnet'),
-  (DEFAULT, 'Øya'),
-  (DEFAULT, 'Tyholt'),
-  (DEFAULT, 'Moholt'),
-  (DEFAULT, 'Ålesund'),
-  (DEFAULT, 'Gjøvik'),
-  (DEFAULT, 'Tunga'),
-  (DEFAULT, 'Valgrinda'),
-  (DEFAULT, 'Olavskvartalet'),
-  (DEFAULT, 'Kunstakademiet');
+  (DEFAULT, 'Gløshaugen', 1),
+  (DEFAULT, 'Dragvoll', 1),
+  (DEFAULT, 'Kalvskinnet', 1),
+  (DEFAULT, 'Øya', 1),
+  (DEFAULT, 'Tyholt', 1),
+  (DEFAULT, 'Moholt', 1),
+  (DEFAULT, 'Ålesund', 1),
+  (DEFAULT, 'Gjøvik', 1),
+  (DEFAULT, 'Tunga', 1),
+  (DEFAULT, 'Valgrinda', 1),
+  (DEFAULT, 'Olavskvartalet', 1),
+  (DEFAULT, 'Kunstakademiet', 1);
